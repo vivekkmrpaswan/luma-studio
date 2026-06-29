1e9f69ceb6399d9070b4f64d158377d085c39729
Vivek Kumar <vivekkmrpaswan@gmail.com>
2026-06-29 12:10:39 +0530

    feat: integrate Sentry for error tracking and monitoring
    - Added Sentry as a dependency in package.json.
    - Created configuration files for Sentry in both edge and server environments (sentry.edge.config.ts, sentry.server.config.ts).
    - Enabled logging and user PII tracking in Sentry configurations.
    - Added multiple image assets to the public directory for use in the application.

---
+++ sentry.edge.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://eeb4b1882937acb1218f7a92d07fbe07@o4511647259230208.ingest.us.sentry.io/4511647266373632",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
